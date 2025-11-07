1ab2c15934117aa77f8fffe303f39e794372b857
Yeon Seok Zach Lee <winterescapade17@snu.ac.kr>
2025-11-07 17:56:47 +0900

    feat: Add Post page for viewing individual blog posts.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { EditPage } from "./pages/EditPage";
 import { MainPage } from "./pages/MainPage";
+import { PostPage } from "./pages/PostPage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className="place-self-center">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/post" element={<PostPage />} />
           <Route path="/edit" element={<EditPage />} />
         </Routes>
       </div>
